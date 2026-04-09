c9a9eab4dd52fdf4e0ad4c4c3e03f982ff56ab66
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-04-09 10:26:51 +0500

    feat: wrap "Get Source Code" button text in a Link component for external navigation

--- src/pages/Frontend/Home/Hero.jsx
+++ src/pages/Frontend/Home/Hero.jsx
@@ -1,6 +1,7 @@
 import { Button } from "antd"
 import sound from "@/assets/faaah.mp3"
 import { useRef } from "react"
+import { Link } from "react-router-dom"
 
 const Hero = () => {
   const soundRef = useRef(null)
@@ -18,7 +19,9 @@ const Hero = () => {
         <h1 className="text-center">{user?.email}</h1>
         <p className='text-center'>We are glad to have you here. Explore our content and enjoy your stay!👇</p>
         <Button type="primary" size="large" className="d-block mx-auto" onClick={playSound}>
-          Get Source Code
+          <Link className="text-decoration-none" to="https://github.com/SufyanAli-7/React-Basic-Setup-Routing-Assignment" target="_blank" rel="noopener noreferrer">
+            Get Source Code
+          </Link>
         </Button>
 
         <audio ref={soundRef} src={sound} />
