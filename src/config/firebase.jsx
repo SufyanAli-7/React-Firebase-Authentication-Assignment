5af5d9a0a745d5abf380113fdc0a34a0da236f32
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-06-05 16:56:31 +0500

    chore: move firebase configuration credentials to environment variables

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDZSoY5dtvZYVJqQ5VLP90ES6X50CS4-jk",
-  authDomain: "react-assignment-auth1.firebaseapp.com",
-  projectId: "react-assignment-auth1",
-  storageBucket: "react-assignment-auth1.firebasestorage.app",
-  messagingSenderId: "1098634792475",
-  appId: "1:1098634792475:web:d905489ba8b920266df2be",
-  measurementId: "G-7JTVXHJQVN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
